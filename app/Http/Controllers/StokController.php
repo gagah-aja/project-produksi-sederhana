7410153f31964ba7d163ea29d26bcb04d023897e
andriansah1 <lanaandriansah29@gmail.com>
2025-07-15 10:27:28 +0700

    stok belum jadi

--- app/Http/Controllers/StokController.php
+++ app/Http/Controllers/StokController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class StokController extends Controller
 {
-    //
+    public function index()
+    {
+        // Contoh tanpa data
+        return view('admin.stok.index');
+
+        // Contoh dengan data
+        // $stok = Stok::all();
+        // return view('admin.stok.index', compact('stok'));
+    }
 }
